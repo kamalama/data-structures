b5ebed27bb7ef46fd2beb30623fe287e726080e8
Hack Reactor Students <kasikamla@gmail.com>
2013-11-13 15:38:42 -0800

    passes reports a size of 2 after adding two items

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,18 +3,19 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    size++;
   };
 
   instance.pop = function(){
   };
 
   instance.size = function(){
-    return 0;
+    return size;
   };
 
   return instance;
