e9e59b3bc13f0f40a438bedb1421e96bdcff0fc3
Hack Reactor Students <kasikamla@gmail.com>
2013-11-14 14:24:23 -0800

    copied over queue.js from functional shared

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,33 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+  instance.head = 0;
+  instance.dequeueCount = 0;
+  instance.storage = {};
+  $.extend(instance, queueMethods);
+
+  // Implement the methods below
+
+  return instance;
 };
 
 var queueMethods = {};
+  queueMethods.enqueue = function(value){
+    this.head++;
+    this.storage[this.head-1] = value;
+  };
+
+  queueMethods.dequeue = function(){
+    if (this.head - this.dequeueCount > 0) {
+      this.dequeueCount++;
+      var tempVal = {
+      "0": this.storage[this.dequeueCount-1]
+      };
+      delete this.storage[this.dequeueCount-1];
+      return tempVal[0];
+    }
+  };
+
+  queueMethods.size = function(){
+    return this.head-this.dequeueCount;
+  };
