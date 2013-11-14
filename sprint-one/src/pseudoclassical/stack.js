16b569535e25cd3201553b41bfbc85e1f6119b43
Hack Reactor Students <kasikamla@gmail.com>
2013-11-14 15:39:20 -0800

    copied over stack.js from prototypal pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,28 @@
 var Stack = function() {
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.len = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+  return instance;
+
 };
+
+var stackMethods = {};
+
+  stackMethods.push = function(value){
+    this.storage[this.len] = value;
+    this.len++;
+  };
+
+  stackMethods.pop = function(){
+  (this.len > 0) && this.len--;
+    return this.storage[this.len];
+  };
+
+  stackMethods.size = function(){
+    return this.len;
+  };
