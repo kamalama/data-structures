b3c39dae7bdc803eb31a02f3e98eafcc56234ab3
Hack Reactor Students <kasikamla@gmail.com>
2013-11-14 10:56:28 -0800

    Copied code from functional/stack.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,28 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional/stack.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var size = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+
+  instance.push = function(value){
+    storage[size] = value;
+    size++;
+  };
+
+  instance.pop = function(){
+    (size > 0) && size--;
+    return storage[size];
+  };
+
+  instance.size = function(){
+    return size;
+  };
+
+  return instance;
 };
 
 var stackMethods = {};
