bc89537878448195fa7fad885d358117ce777855
Hack Reactor Students <kasikamla@gmail.com>
2013-11-14 13:55:37 -0800

    copied queue from functional patter

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,36 @@
 var makeQueue = function(){
   // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+  var head = 0;
+  var dequeueCount = 0;
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  // Implement the methods below
+
+  instance.enqueue = function(value){
+    head++;
+    storage[head-1] = value;
+  };
+
+  instance.dequeue = function(){
+    if (head - dequeueCount > 0) {
+      dequeueCount++;
+      var tempVal = {
+      "0": storage[dequeueCount-1]
+      };
+      console.log(storage[dequeueCount-1]);
+      delete storage[dequeueCount-1];
+      return tempVal[0];
+    }
+  };
+
+  instance.size = function(){
+    return head-dequeueCount;
+  };
+
+  return instance;  
 };
 
 var queueMethods = {};
