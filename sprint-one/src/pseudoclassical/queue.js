f09dc7e9d30aaa6e6c109a25ca8f99ce30d005c2
Hack Reactor Students <kasikamla@gmail.com>
2013-11-14 15:47:50 -0800

    completed all tests for pseudoclassical, queue.js

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,28 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+
+  this.head = 0;
+  this.dequeueCount = 0;
+  this.storage = {};
+
 };
+
+Queue.prototype.enqueue = function(value){
+  this.head++;
+  this.storage[this.head-1] = value;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.head - this.dequeueCount > 0) {
+    this.dequeueCount++;
+    var tempVal = {
+    "0": this.storage[this.dequeueCount-1]
+    };
+    delete this.storage[this.dequeueCount-1];
+    return tempVal[0];
+  }
+};
+
+Queue.prototype.size = function(){
+  return this.head-this.dequeueCount;
+};
+
