cd24eea34999008500c91ed49a0ab5fac040a6b5
Hack Reactor Students <kasikamla@gmail.com>
2013-11-15 15:39:57 -0800

    Passes test for treeMethods.addChild

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -11,5 +11,25 @@ describe("tree", function() {
     expect('value' in tree).toBe(true);
   });
 
-  // Add more tests here to test the functionality of tree.
+// Add more tests here to test the functionality of tree.
+  // it("method addChild should take a value", function() {
+  //   var tree2 = "test";
+  //   tree.addChild(tree2);
+  //   expect(tree.addChild()).toHaveBeenCalledWith(tree2);
+  // });
+
+  it("should add that node as a child of the tree using method addChild", function() {
+    var tree2 = "tree2";
+    tree.addChild(tree2);
+    expect(tree.children).toContain("tree2");
+
+  });
 });
+
+
+
+
+// A tree class, in functional with shared methods style, with the following properties:
+// .children property, an array containing a number of subtrees
+// .addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
+// A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,14 +2,23 @@ var makeTree = function(){
   var newTree = {};
   newTree.value = undefined;
   newTree.children = undefined;
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(child){
+  if(this.children === undefined){
+    this.children = [child];
+  }
+  else {
+    this.children.push(child);
+  }
 };
 
 treeMethods.contains = function(){
 };
+
